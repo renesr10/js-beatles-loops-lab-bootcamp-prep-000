3ddcb3746144c0bab4bdfa3d933fd6ca48e3f652
Rene Santana <renesantana10@gmail.com>
2018-08-29 14:38:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array[]; { for (var i = 0; i < 5; i++) {
+  array = array + i;
+  }
+  }
+}
+
+function johnLennonFacts(facts) {
+  
+}
+function iLoveTheBeatles(n) {
+  
+}
