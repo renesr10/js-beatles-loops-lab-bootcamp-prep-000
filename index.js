3eff13986d30bab83e709137a951cd7269cab5ea
Rene Santana <renesantana10@gmail.com>
2018-08-29 16:07:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) { let array[]
 {for (let i = 0; i < 5; i++) {
-  if (i === ) { array.push("I am 1 strange loop."); } else  { array.push(`I am ${i} strange loops.`); } }
+  if (i === ) { array.push(`${} plays ${}); } else  { array.push(`I am ${i} strange loops.`); } }
   return array 
  }
 }
