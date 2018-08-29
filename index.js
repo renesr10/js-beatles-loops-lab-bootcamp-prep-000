b49337e51c6034254d2def7f74e2c8b54de6c6da
Rene Santana <renesantana10@gmail.com>
2018-08-29 16:12:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) { let array[]; 
-{for (let i = 0; i < musicians.length; i++) {
+for (let i = 0; i < musicians.length; i++) {
    { array.push(musicians[i] + ' plays ' + instruments[i]) } }
   return array; 
- }
 }
 
