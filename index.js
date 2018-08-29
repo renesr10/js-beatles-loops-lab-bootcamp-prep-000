416c7519b6f7e130956c7c206cd8f3fc22888328
Rene Santana <renesantana10@gmail.com>
2018-08-29 16:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments) { let array[ ]; for (let i = 0; i < musicians.length; i++) {
-   { array.push(musicians[i] + ' plays ' + instruments[i]) } }
+function theBeatlesPlay(musicians, instruments) { var array[ ]; for (let i = 0; i < musicians.length; i++) {
+   { array.push(musicians[i] + ' plays ' + instruments[i]) 
   return array; 
 }
 
